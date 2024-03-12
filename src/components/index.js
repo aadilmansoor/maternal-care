8af3acfbbf335cbd2f664fd9879eab904a667053
MOHAMMAD FAWAS CM <favasmcz@gmail.com>
2024-03-12 17:08:36 +0530

    added hero section in provider dashboard and edited provider register page

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from "./footer/Footer";
 export { default as ServicesCards } from "./servicecards/ServicesCards";
 export { default as UserHeader } from "./userheader/UserHeader";
 export { default as UserHero } from "./userhero/UserHero";
+export {default as ProviderHero} from "./providerHero/ProviderHero"
