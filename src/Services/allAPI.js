aabebfbf2ed9bd6e0c0354e5431cc9c6be326fe1
Aadil Mansoor <aadilmansoor.am@gmail.com>
2024-03-13 14:44:35 +0530

    integrated registration pages with backend

--- src/Services/allAPI.js
+++ src/Services/allAPI.js
@@ -1,7 +1,22 @@
-import { commonAPI } from "./commonAPI"
-import { base_URL } from "./serverURL"
+import { commonAPI } from "./commonAPI";
+import { base_URL } from "./serverURL";
 
-//register
-export const registerAPI=async(reqBody)=>{
-    return await commonAPI('POST',`${base_URL}/maternalcare/user/register`,reqBody,"")
-}
+//register user
+export const registerAPI = async (reqBody) => {
+  return await commonAPI(
+    "POST",
+    `${base_URL}/maternalcare/user/register`,
+    reqBody,
+    ""
+  );
+};
+
+//register service provider
+export const registerProviderAPI = async (reqBody) => {
+  return await commonAPI(
+    "POST",
+    `${base_URL}/maternalcare/serviceProvider/register`,
+    reqBody,
+    { "Content-Type": "multipart/form-data" }
+  );
+};
--- src/pages/Service_Provider/SpRegister.jsx
+++ src/pages/Service_Provider/SpRegister.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { useState } from "react";
 import { Col, Container, Form, Row } from "react-bootstrap";
+import { registerProviderAPI } from "../../Services/allAPI";
+import { toast, ToastContainer } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 function SpRegister() {
+  const navigate = useNavigate();
+  const [providerDetails, setProviderDetails] = useState({
+    username: "",
+    password: "",
+    email: "",
+    mobile: "",
+    service: "",
+    specialization: "",
+    qualification: "",
+    exp_year: "",
+    experience_crt: {},
+    rate: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await registerProviderAPI(providerDetails);
+    console.log(result);
+    if (result.status === 200) {
+      toast.success("Registration Successful", {
+        autoClose: 2000,
+      });
+      setTimeout(() => {
+        navigate("/");
+      }, 2500);
+    } else {
+      console.log(result);
+    }
+  };
+
   return (
     <>
       <div className="d-flex justify-content-center align-items-center mt-3 mb-5">
@@ -24,70 +57,139 @@ function SpRegister() {
                   <h3 className=" roboto-bold text-center mt-3 text-primary">
                     Service Provider Registration
                   </h3>
-                  <input
-                    style={{ borderRadius: "50px" }}
-                    type="text"
-                    className="form-control mt-3"
-                    placeholder="Username"
-                  />
-                  <input
-                    style={{ borderRadius: "50px" }}
-                    type="email"
-                    className="form-control mt-3"
-                    placeholder="Email Address"
-                  />
-                  <input
-                    style={{ borderRadius: "50px" }}
-                    type="password"
-                    className="form-control mt-3"
-                    placeholder="Password"
-                  />
-                  <input
-                    style={{ borderRadius: "50px" }}
-                    type="tel"
-                    className="form-control mt-3"
-                    placeholder="Phone Number"
-                  />
-                  <input
-                    type="text"
-                    placeholder="service"
-                    style={{ borderRadius: "50px" }}
-                    className="form-control mb-3 mt-2"
-                  />
-                  <input
-                    type="text"
-                    placeholder="specialization"
-                    style={{ borderRadius: "50px" }}
-                    className="form-control mb-3 mt-2"
-                  />
-                  <input
-                    type="text"
-                    placeholder="qualification"
-                    style={{ borderRadius: "50px" }}
-                    className="form-control mb-3 mt-2"
-                  />
-                  <input
-                    type="text"
-                    placeholder="Experience Year"
-                    style={{ borderRadius: "50px" }}
-                    className="form-control mb-3 mt-2"
-                  />
-                  <Form.Group controlId="formFile" className="mb-3 w-100"  >
-                    <Form.Label className="ms-2">Experience Certificate</Form.Label>
-                    <Form.Control type="file" accept=".pdf" style={{ borderRadius: "50px" }}/>
-                  </Form.Group>
-                  <input
-                    type="rate"
-                    placeholder="Rate"
-                    style={{ borderRadius: "50px" }}
-                    className="form-control mb-3 mt-2"
-                  />
-                  <button
-                    className="w-100 mt-3 btn btn-danger"
-                    style={{ borderRadius: "50px" }}
-                  >
-                    REGISTER
-                  </button>
+                  <Form onSubmit={handleSubmit}>
+                    <input
+                      style={{ borderRadius: "50px" }}
+                      type="text"
+                      className="form-control mt-3"
+                      placeholder="Username"
+                      onChange={(e) =>
+                        setProviderDetails({
+                          ...providerDetails,
+                          username: e.target.value,
+                        })
+                      }
+                    />
+                    <input
+                      style={{ borderRadius: "50px" }}
+                      type="email"
+                      className="form-control mt-3"
+                      placeholder="Email Address"
+                      onChange={(e) =>
+                        setProviderDetails({
+                          ...providerDetails,
+                          email: e.target.value,
+                        })
+                      }
+                    />
+                    <input
+                      style={{ borderRadius: "50px" }}
+                      type="password"
+                      className="form-control mt-3"
+                      placeholder="Password"
+                      onChange={(e) =>
+                        setProviderDetails({
+                          ...providerDetails,
+                          password: e.target.value,
+                        })
+                      }
+                    />
+                    <input
+                      style={{ borderRadius: "50px" }}
+                      type="tel"
+                      className="form-control mt-3"
+                      placeholder="Phone Number"
+                      onChange={(e) =>
+                        setProviderDetails({
+                          ...providerDetails,
+                          mobile: e.target.value,
+                        })
+                      }
+                    />
+                    <input
+                      type="text"
+                      placeholder="service"
+                      style={{ borderRadius: "50px" }}
+                      className="form-control mb-3 mt-2"
+                      onChange={(e) =>
+                        setProviderDetails({
+                          ...providerDetails,
+                          service: e.target.value,
+                        })
+                      }
+                    />
+                    <input
+                      type="text"
+                      placeholder="specialization"
+                      style={{ borderRadius: "50px" }}
+                      className="form-control mb-3 mt-2"
+                      onChange={(e) =>
+                        setProviderDetails({
+                          ...providerDetails,
+                          specialization: e.target.value,
+                        })
+                      }
+                    />
+                    <input
+                      type="text"
+                      placeholder="qualification"
+                      style={{ borderRadius: "50px" }}
+                      className="form-control mb-3 mt-2"
+                      onChange={(e) =>
+                        setProviderDetails({
+                          ...providerDetails,
+                          qualification: e.target.value,
+                        })
+                      }
+                    />
+                    <input
+                      type="number"
+                      placeholder="Experience Year"
+                      style={{ borderRadius: "50px" }}
+                      className="form-control mb-3 mt-2"
+                      onChange={(e) =>
+                        setProviderDetails({
+                          ...providerDetails,
+                          exp_year: e.target.value,
+                        })
+                      }
+                    />
+                    <Form.Group controlId="formFile" className="mb-3 w-100">
+                      <Form.Label className="ms-2">
+                        Experience Certificate
+                      </Form.Label>
+                      <Form.Control
+                        type="file"
+                        accept=".pdf"
+                        style={{ borderRadius: "50px" }}
+                        onChange={(e) =>
+                          setProviderDetails({
+                            ...providerDetails,
+                            experience_crt: e.target.files[0],
+                          })
+                        }
+                      />
+                    </Form.Group>
+                    <input
+                      type="rate"
+                      placeholder="Rate"
+                      style={{ borderRadius: "50px" }}
+                      className="form-control mb-3 mt-2"
+                      onChange={(e) =>
+                        setProviderDetails({
+                          ...providerDetails,
+                          rate: e.target.value,
+                        })
+                      }
+                    />
+                    <button
+                      type="submit"
+                      className="w-100 mt-3 btn btn-danger"
+                      style={{ borderRadius: "50px" }}
+                    >
+                      REGISTER
+                    </button>
+                  </Form>
                 </Col>
               </Row>
             </Col>
@@ -95,12 +197,13 @@ function SpRegister() {
               <img
                 className=" mb-3"
                 src="https://dailynclexchallenge.com/static/img/blog/nurse-with-pregnant.png"
-                alt="serviceprovider2"
+                alt="service provider2"
                 style={{ height: "700px", width: "100%" }}
               />
             </Col>
           </Row>
         </Container>
+        <ToastContainer position="bottom-right" />
       </div>
     </>
   );
