9a5344b908d47a92edf0188d0b97428ec18a8d49
Aadil Mansoor <aadilmansoor.am@gmail.com>
2024-05-09 10:29:46 +0530

    removed / in base url

--- src/Services/serverURL.js
+++ src/Services/serverURL.js
@@ -1,2 +1,2 @@
 // export const base_URL='http://localhost:4000'
-export const base_URL = "https://maternal-care-backend.vercel.app/";
+export const base_URL = "https://maternal-care-backend.vercel.app";
